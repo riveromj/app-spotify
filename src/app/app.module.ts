68c50b0e305513eb2c9178e85eb260e1331f2fb7
Marigen Rivero <marigen@gmail.com>
2022-01-14 14:10:19 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //TODO: declaraciones , componentes, directivas y pipes
+    AppComponent,
   ],
-  imports: [
+  imports: [//TODO: solo se importan otros modulos
     BrowserModule,
     AppRoutingModule
   ],
