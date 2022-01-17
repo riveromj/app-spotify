cb3e2afcd1c25b8f1b9162ae7e7c0e404b08b10d
Marigen Rivero <marigen@gmail.com>
2022-01-17 11:00:24 +0100

    add track page

---
+++ src/app/modules/tracks/pages/track-page/track-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-track-page',
+  templateUrl: './track-page.component.html',
+  styleUrls: ['./track-page.component.css']
+})
+export class TrackPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
