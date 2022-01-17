044b32df98f3c860302dd085aaeb6d20a4a699c6
Marigen Rivero <marigen@gmail.com>
2022-01-17 15:58:27 +0100

    send data from section to card

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TracksModel } from '@core/models/tracks.model';
+
 
 @Component({
   selector: 'app-card-player',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-player.component.css']
 })
 export class CardPlayerComponent implements OnInit {
+  @Input () mode: 'small' | 'big' = 'small';
+  @Input () track: TracksModel = {
+    name: '',
+    album: '',
+    cover: '',
+    url: '',
+    _id: ''
+  };
 
   constructor() { }
 
