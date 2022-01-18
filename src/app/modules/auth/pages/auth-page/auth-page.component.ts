0e8131151a0e1af44c57ee6512ba851d2af92d79
Marigen Rivero <marigen@gmail.com>
2022-01-18 11:32:17 +0100

    login form capture

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPageComponent implements OnInit {
 
+  formLogin: FormGroup = new FormGroup({});
+  errorSession: string ='';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('',[ Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.minLength(12)])
+    })
+  }
+  sendLogin(){
+    const body = this.formLogin.value;
+    console.log(body);
   }
 
 }
